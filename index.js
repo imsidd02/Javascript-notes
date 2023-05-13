9f84f780407df7efb4dd5ab32e7beb1a75ca5b1d
siddharth singh <imsidd02@gmail.com>
2023-05-13 15:05:22 +0530

    practice

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello brother");
+let score = 10;
+console.log(score);
+score = 15;
+console.log(score);
+
+const ram ="shyam";
+console.log(ram);
+
+
+let itsHot = true;
+console.log(itsHot);
+itsHot = false;
+console.log(itsHot);
